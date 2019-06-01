e7edd7c70cb012f0d3e45e1e1c3f5e87686008fa
adisonn88 <adisonn88@wp.pl>
2019-06-01 12:13:27 +0200

    new components form

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form'
+import Result from './Result'
+
+function App() {
+  return (
+    <div className="App">
+    <Form />
+    <Result />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <form>
+            <input type="text"></input>
+            <button>Wyszukaj miasta</button>
+        </form>
+    )
+}
+
+export default Form 
