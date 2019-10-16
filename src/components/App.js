359bf8e46580d34aa5277b317e002bc40beb2a37
adrian-glowienka <45940806+adrian-glowienka@users.noreply.github.com>
2019-10-16 15:58:14 +0200

    Add new functions

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,71 @@ import React from 'react';
 import Form from './Form'
 import Result from './Result'
 
-function App() {
+class App extends React.Component() {
+
+    state = {
+      value: "",
+      date:"",
+      city:"",
+      sunrise:"",
+      sunset:"",
+      temp:"",
+      pressure:"",
+      wind:"",
+      err: false
+    }
+
+handleInputChange = (e) => {
+  this.setState({
+    value: e.target.value
+  })
+}
+
+handleCitiSubmit = (e) => {
+  e.preventDefault()
+  const API = `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}`
+
+    fetch(API)
+      .then(response =>{
+        if(response.ok){
+          return response
+        }
+        throw Error("Błąd")
+      })
+      .then(response => response.json())
+      .then(data => {
+        const time = new Date().toLocaleString()
+        this.setState({
+          err: false,
+          date: time,
+          sunrise: data.sys.sunrise,
+          sunset: data.sys.sunset,
+          temp: data.main.temp,
+          pressure: data.main.pressure,
+          wind: data.main.wind.speed,
+          city: this.state.value
+        })
+      })
+      .catch(err =>{
+        this.setState({
+          err: true,
+          city: this.state.value
+        })
+      })
+}
+
+render(){
   return (
-    <div className="App">
-    <Form />
-    <Result />
-    </div>
-  );
+      <div className="App">
+        <Form 
+          value={this.state.value} 
+          change={this.handleInputChange} 
+          submit={this.handleCitiSubmit}
+        />
+        <Result weather={this.state}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 
-const Result = () => {
+const Result = (props) => {
+
+    const { err, city, sunrise, sunset, temp, oressure, wind, err } = props.weather;
+
+    let content = null;
+
+    if(!err && city) {
+       content = (
+           <div>{`Wyszukiwanie dla miasta ${city}`}</div>
+       ) 
+    }
+
     return (
-        <div>Miasto, pogoda</div>
+        <div className="result">
+            {err ? `Nie znaleziono w bazie miasta ${city}` : content}
+        </div>
     )
 }
 
